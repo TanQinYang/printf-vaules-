9602ddf343689fe77bdadf672e985387f4acb540
TanQinYang <108197858+TanQinYang@users.noreply.github.com>
2022-07-02 16:46:38 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int n; /*declaration*/
+   n=2;  /*instruction*/
+   int m; /*declaration*/
+   m=-5;  /*instruction*/
+   printf("The value is:%d,%d\n",n,m);     /*int对应%d*/
+   printf("The sum of :%d",n+m);     /*一个值对应一个%d*/
+    return 0;
+}
